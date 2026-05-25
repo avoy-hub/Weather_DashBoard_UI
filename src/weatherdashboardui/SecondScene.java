baebd6a5bf8b3ccaa97f7a280ef5a454413c5603
Your Name <mahathirmohammad842@gmail.com>
2026-05-25 06:36:13 +0600

    Added SecondScene.java (Mahathir)

---
+++ src/weatherdashboardui/SecondScene.java
@@ -0,0 +1,42 @@
+package WeatherDashBoardUI;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class SecondScene {
+
+    public void start(Stage stage, String cityName) {
+
+        // Labels
+        Label title = new Label("City Weather");
+        Label city = new Label("City : " + cityName);
+        Label temp = new Label("Temperature : -- °C");
+        Label humidity = new Label("Humidity : -- %");
+        Label condition = new Label("Condition : --");
+        Label wind = new Label("Wind Speed : -- m/s");
+
+        // Button
+        Button submitBtn = new Button("Submit");
+
+        // Layout
+        VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(title, city, temp, humidity, condition, wind, submitBtn);
+
+        // Scene
+        Scene scene2 = new Scene(root, 300, 400);
+
+//        // Button Action -> ThirdScene এ যাবে
+//        submitBtn.setOnAction(e -> {
+//            ThirdScene third = new ThirdScene();
+//            third.start(stage);
+//        });
+
+        stage.setScene(scene2);
+        stage.show();
+    }
+}
