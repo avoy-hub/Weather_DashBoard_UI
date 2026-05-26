15310a77f3bae7fbab0f385b944a323ae01010cc
jim-32 <jobayermahmudjim@gmail.com>
2026-05-26 08:42:26 +0600

    Today i modified the last part of the scene

--- src/weatherdashboardui/ThirdScene.java
+++ src/weatherdashboardui/ThirdScene.java
@@ -1,11 +1,8 @@
 package weatherdashboardui;
 
-import static javafx.application.Application.launch;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -26,10 +23,9 @@ public class ThirdScene extends WeatherDashBoardUI {
         Label barisal = new Label("Barisal : 30°C");
         Label mymensingh = new Label("Mymensingh : 28°C");
 
-        TextField cityField = new TextField();
-        cityField.setPromptText("Enter Division Name");
-
-        Button searchBtn = new Button("Search");
+        Label summary = new Label(
+                "Summary: Bangladesh weather is mostly warm with scattered clouds across all divisions."
+        );
 
         VBox root = new VBox(10);
         root.setAlignment(Pos.CENTER);
@@ -44,28 +40,12 @@ public class ThirdScene extends WeatherDashBoardUI {
                 rajshahi,
                 barisal,
                 mymensingh,
-                cityField,
-                searchBtn
+                summary
         );
-
-        searchBtn.setOnAction(e -> {
-
-            String division = cityField.getText();
-
-            title.setText("Searching Weather for : " + division);
-
-        });
-
         Scene scene = new Scene(root, 400, 500);
 
         stage.setTitle("Weather Dashboard");
-
         stage.setScene(scene);
-
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
